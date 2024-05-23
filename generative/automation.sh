3c4a50d4e6e3187c33755c3af46ca3c4ebbebd68
Kyoosik Lee <kyoosik.lee@gmail.com>
2024-05-23 14:33:11 +0000

    fix: delete unneccesary annotation

--- generative/automation.sh
+++ generative/automation.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# Define arrays for CLS and PREPROMPT
 CLASSES=("Nurse" "Designer" "Florist" "Hairdresser" "Computer Programmer" "Construction Worker" "HollywoodActor" "rapper" "terrorist")
 PREPROMPTS=("A" "B" "C")
 
-# Iterate over each combination of CLS and PREPROMPT
 for CLS in "${CLASSES[@]}"; do
   for PREPROMPT in "${PREPROMPTS[@]}"; do
-    # Execute commands
     python main.py --cls "$CLS" --debias-method single --lam 0 --preprompt "$PREPROMPT"
     python main.py --cls "$CLS" --debias-method single --lam 500 --preprompt "$PREPROMPT"
     python main.py --cls "$CLS" --debias-method pair --lam 500 --preprompt "$PREPROMPT"
